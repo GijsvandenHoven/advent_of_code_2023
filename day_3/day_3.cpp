b1038f0705dbf8979118d6db0e6b6209027e2732
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-02 17:39:30 +0100

    create template and day 3 file

---
+++ day_3/day_3.cpp
@@ -0,0 +1,40 @@
+#define STR(x) #x
+#define FILE_PATH(num)  "day_" STR(num) "/day" STR(num) ".txt"
+
+#include <iostream>
+#include <fstream>
+
+#define DAY 3
+#define FILE FILE_PATH(DAY)
+
+namespace v1 {
+    int solve(std::ifstream& text);
+}
+
+namespace v2 {
+    int solve(std::ifstream& text);
+}
+
+int main () {
+
+    {
+        std::ifstream input(FILE);
+        auto result = v1::solve(input);
+        std::cout << result;
+    }
+    {
+        std::ifstream input(FILE);
+        auto result = v2::solve(input);
+        std::cout << result;
+    }
+
+    return 0;
+}
+
+int v1::solve(std::ifstream& text) {
+    return 0;
+}
+
+int v2::solve(std::ifstream& text) {
+    return 0;
+}
