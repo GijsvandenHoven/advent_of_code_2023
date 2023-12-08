4c3c25a027f1759cabaeb6ac372d042bdcfbc4bf
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-08 21:20:17 +0100

    Day 8 general-ish V2 solution that accounts for offset in LCM as well even though it is not necessary for the puzzle input

--- day_8/day_8.hpp
+++ day_8/day_8.hpp
@@ -21,6 +21,10 @@ class Instructions {
     std::istringstream instructionScanner;
 public:
 
+    int pos() {
+        return static_cast<int>(instructionScanner.tellg());
+    }
+
     Direction getDirection() {
         int c = instructionScanner.get();
 
@@ -98,6 +102,14 @@ public:
 
         return iter->second.get();
     }
+
+    void get_nodes_ending_with(char c, std::vector<const NetworkNode *>& out) const {
+        std::for_each(network.begin(), network.end(), [&out, c](auto& pair) {
+            if (pair.first.back() == c) {
+                out.push_back(pair.second.get());
+            }
+        });
+    }
 };
 
 std::ostream& operator<<(std::ostream& os, const Network& n) {
@@ -164,6 +176,7 @@ public:
         int stepCount = 0;
         auto* current = network.get_node("AAA");
         while (current->label != "ZZZ") {
+            stepCount ++; // step taken, where do we end up?
             auto direction = instructions.getDirection();
             switch(direction) {
                 case Direction::LEFT:
@@ -175,8 +188,6 @@ public:
                 default:
                     throw std::logic_error("Literally how, this enum is a bool.");
             }
-
-            stepCount ++;
         }
 
         reportSolution(stepCount);
@@ -184,7 +195,77 @@ public:
 
     void v2() const override {
 
-        reportSolution(0);
+        std::vector<const NetworkNode *> starts;
+        std::vector<const NetworkNode *> ends;
+        network.get_nodes_ending_with('A', starts);
+        network.get_nodes_ending_with('Z', ends);
+
+        std::vector<std::pair<int64_t, int64_t>> cycleValues; // first and last reported cycle.
+        for (auto start : starts) {
+            int steps_taken = 0;
+            std::vector<std::tuple<int, std::string, int>> z_node_hits;
+            Instructions instructions(instructions_string); // create fresh each time, mutating this between nodes is a bad idea.
+            auto node = start;
+
+            bool cycleFound = false;
+            while (! cycleFound) {
+                node = findNextZ(instructions, node, steps_taken);
+                int instruction_pos = instructions.pos();
+
+                auto predicate = [&instruction_pos, &node](auto& item) {
+                    return instruction_pos == std::get<0>(item) && node->label == std::get<1>(item);
+                };
+
+                // always add it, even if it's a 'duplicate' as per the predicate,
+                // we want at least 2 in the vec for cycle calculation with an 'offset' (although if there is an offset, we cannot LCM)
+                z_node_hits.emplace_back(instructions.pos(), node->label, steps_taken);
+
+                if (z_node_hits.end() != std::find_if(z_node_hits.begin(), z_node_hits.end(), predicate)) {
+                    cycleFound = true;
+                }
+            }
+
+            // Check for funny business, not part of the puzzle input but...
+            if (z_node_hits.size() != 2) {
+                int diff = std::get<2>(z_node_hits[1]) - std::get<2>(z_node_hits[0]);
+                int prev = 0;
+                for (auto& [a, b, c] : z_node_hits) {
+                    if (prev != 0 && c - prev != diff) {
+                        throw std::logic_error("Can't solve this: Only works for predictable cycles such that some form of LCM can be used.");
+                    }
+                    prev = c;
+                }
+            }
+
+            // znodehits has at least 2 members due to how the cycle seeker works.
+            cycleValues.emplace_back( std::get<2>(z_node_hits[0]), std::get<2>(z_node_hits.back()) );
+        }
+
+        bool hasOffset = false;
+        for (auto& [first, last] : cycleValues) {
+            auto x = first / last;
+            auto y = static_cast<double>(first) / static_cast<double>(last);
+            if (static_cast<double>(x) != y) {
+                hasOffset = true;
+            }
+        }
+
+        int64_t result;
+        if (hasOffset) {
+            result = stupid_lcm(cycleValues);
+        } else {
+            bool _1 = true;
+            for (auto& [first, _] : cycleValues) {
+                if (_1) {
+                    result = first;
+                    _1 = false;
+                } else {
+                    result = std::lcm(result, first);
+                }
+            }
+        }
+
+        reportSolution(result);
     }
 
     void parseBenchReset() override {
@@ -195,6 +276,62 @@ public:
 private:
     std::string instructions_string;
     Network network;
+
+    // problem 2 helper func
+    static const NetworkNode * findNextZ(Instructions& i, const NetworkNode * from, int& stepCount) {
+        auto current = from;
+        do {
+            stepCount++;
+            auto dir = i.getDirection();
+            if (dir == Direction::LEFT) {
+                current = current->left();
+            } else {
+                current = current->right();
+            }
+            // std::cout << "\t\tstep " << stepCount << " go " << (dir == Direction::LEFT ? 'L' : 'R') << " to " << current->label << "\n";
+        } while (current->label.back() != 'Z');
+
+        return current;
+    }
+
+    static int64_t stupid_lcm(const std::vector<std::pair<int64_t, int64_t>>& cycleValues) {
+        std::cout << "Warn: LCM will be computed in linear time, presumably due to presence of offsets.\n";
+        std::vector<int64_t> counts;
+        std::vector<int64_t> sizes;
+        std::cout << "WE ARE WORKING WITH THIS:\n";
+        for (auto& [start, end] : cycleValues) {
+            std::cout << "S: " << start << ", E: " << end << ", thus C: " << (end - start) << "\n";
+            counts.push_back(start);
+            sizes.push_back(end-start);
+        }
+
+        int at = 0;
+        uint32_t iter = 0; // intentionally 32 byte, we want to report every power of 2 and this overflowing keeps reporting a bit more frequent.
+        while ( counts.back() != counts[0] ) {
+            iter++;
+            auto& a = counts[at];
+            auto& b = counts[at+1];
+
+            if (a == b) {
+                // std::cout << "@" << at << ": " << a << " == " << b << "\n";
+                at++;
+            } else if (a > b) {
+                b += sizes[at+1];
+                at = 0; // invalidated, check again
+            } else { // a < b
+                a += sizes[at];
+                at = 0;
+            }
+
+            if ((iter & (iter - 1)) == 0) {
+                std::cout << iter << " - ";
+                for (auto v : counts) std::cout << v << ", ";
+                std::cout << "\n";
+            }
+        }
+
+        return counts[0];
+    }
 };
 
 #undef DAY
