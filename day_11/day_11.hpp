1687cea1ad451712be635c5d2d05eced9876420f
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-11 19:21:47 +0100

    day 11

--- day_11/day_11.hpp
+++ day_11/day_11.hpp
@@ -9,28 +9,127 @@
 
 NAMESPACE_DEF(DAY) {
 
-    CLASS_DEF(DAY) {
-    public:
-        DEFAULT_CTOR_DEF(DAY)
+struct Galaxy {
+    int x;
+    int y;
 
-        void parse(std::ifstream &input) override {
+    Galaxy(int x, int y) : x(x), y(y) {}
 
+    [[nodiscard]] int manhattanDistance(const Galaxy& other) const {
+        return std::abs(x - other.x) + std::abs(y - other.y);
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const Galaxy& g) {
+    os << "Galaxy at { " << g.x << ", " << g.y << "\n";
+    return os;
+}
+
+constexpr int P2_GALAXY_EXPANSION_FACTOR = 1'000'000;
+
+CLASS_DEF(DAY) {
+public:
+    DEFAULT_CTOR_DEF(DAY)
+
+    // Assumes a rectangular input; Every line should have the same amount of columns.
+    void parse(std::ifstream &input) override {
+        int columns = 0;
+        while (input.get() != '\n') {
+            columns++;
+        }
+        input.clear();
+        input.seekg(0);
+
+        std::vector<int> columnGalaxyCount(columns);
+
+        int x = 0;
+        int y_1 = 0;
+        int y_2 = 0; // need to separately track the 1 million unit expansion of problem 2.
+
+        int c;
+        int rowCount = 0;
+        while ((c = input.get()) != EOF) {
+            switch(c) {
+                case '\n':
+                    y_1 += (rowCount == 0 ? 2 : 1); // rows count double if no galaxies.
+                    y_2 += (rowCount == 0 ? P2_GALAXY_EXPANSION_FACTOR : 1);
+                    x = 0;
+                    rowCount = 0;
+                    continue; // do not update column and x.
+                case '#':
+                    rowCount++;
+                    columnGalaxyCount[x]++;
+                    galaxies.emplace_back(x, y_1);
+                    veryExpandedGalaxies.emplace_back(x, y_2);
+                    break;
+                default:
+                    break;
+            }
+            x++;
         }
 
-        void v1() const override {
-            reportSolution(0);
+        { // problem 1 error correction
+            // correct coordinates for columns
+            int offset = 0;
+            for (int i = 0; i < columnGalaxyCount.size(); ++i) {
+                if (columnGalaxyCount[i] == 0) {
+                    // every galaxy after this X should be offset by 1 on X for this expansion.
+                    std::for_each(galaxies.begin(), galaxies.end(), [i, offset](auto &galaxy) {
+                        if (galaxy.x > i + offset) {
+                            galaxy.x++;
+                        }
+                    });
+                    offset++; // so stretched galaxies do not get stretched again for columns that were originally past them.
+                }
+            }
+        }
+        { // problem 1 error correction
+            // correct coordinates for columns
+            int offset = 0;
+            for (int i = 0; i < columnGalaxyCount.size(); ++i) {
+                if (columnGalaxyCount[i] == 0) {
+                    // every galaxy after this X should be offset by 1 on X for this expansion.
+                    std::for_each(veryExpandedGalaxies.begin(), veryExpandedGalaxies.end(), [i, offset](auto &galaxy) {
+                        if (galaxy.x > i + offset) {
+                            galaxy.x += P2_GALAXY_EXPANSION_FACTOR - 1;
+                        }
+                    });
+                    offset+= P2_GALAXY_EXPANSION_FACTOR - 1; // so stretched galaxies do not get stretched again for columns that were originally past them.
+                }
+            }
+        }
+    }
+
+    void v1() const override {
+        int manhattanSum = 0;
+        for (int i = 0; i < galaxies.size(); ++i) {
+            for (int j = i+1; j < galaxies.size(); ++j) {
+                manhattanSum += galaxies[i].manhattanDistance(galaxies[j]);
+            }
         }
 
-        void v2() const override {
-            reportSolution(0);
+        reportSolution(manhattanSum);
+    }
+
+    void v2() const override {
+        int64_t manhattanSum = 0;
+        for (int i = 0; i < veryExpandedGalaxies.size(); ++i) {
+            for (int j = i+1; j < veryExpandedGalaxies.size(); ++j) {
+                manhattanSum += veryExpandedGalaxies[i].manhattanDistance(veryExpandedGalaxies[j]);
+            }
         }
 
-        void parseBenchReset() override {
+        reportSolution(manhattanSum);
+    }
 
-        }
+    void parseBenchReset() override {
 
-    private:
-    };
+    }
+
+private:
+    std::vector<Galaxy> galaxies;
+    std::vector<Galaxy> veryExpandedGalaxies; // problem 2. since we do not want to mutate the vector, we should parse in the 2 variants.
+};
 
 } // namespace
 
