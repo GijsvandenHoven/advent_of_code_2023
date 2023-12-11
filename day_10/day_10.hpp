49e85d971232832b096219a4e5420ba1975ecbcb
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-11 03:55:02 +0100

    debug prints

--- day_10/day_10.hpp
+++ day_10/day_10.hpp
@@ -262,11 +262,14 @@ public:
     }
 
     void v2() const override {
+        static int count = 0;
+        std::cout << count++ << "\n";
         Maze<PipeSegment> explodedMaze;
         explodeMaze(explodedMaze);
 
         Maze<SearchablePipeSegment> BFSWorksheet; // fully blank maze, to be painted by the loop detection process.
 
+        std::cout << "make work sheet\t";
         BFSWorksheet.reserve(explodedMaze.size());
         for (auto& row : explodedMaze) {
             BFSWorksheet.emplace_back();
@@ -275,14 +278,17 @@ public:
             }
         }
 
+        std::cout << "make work sheet\t";
         auto BFSWorksheetOuter = BFSWorksheet; // copy
         auto BFSWorksheetInner = std::move(BFSWorksheet); // and move.
 
+        std::cout << "double BFS loop check\t";
         doDoubleLoopCheckOnExplodedMaze(explodedMaze, BFSWorksheetOuter, BFSWorksheetInner);
-
+        std::cout << "BFS\t";
         BFSReachableTiles(BFSWorksheetOuter);
         BFSReachableTiles(BFSWorksheetInner);
 
+        std::cout << "Shrink\t";
         // for every 'virtual' tile, the 2x2 block representing one tile in the exploded maze.
         // Count it as explored if _either_ of the worksheets could reach it.
         // Conversely, if neither worksheet could reach it, it is unreachable.
@@ -331,35 +337,7 @@ public:
             }
         }
 
-        Maze<SearchablePipeSegment> triviallyReachable;
-        for (auto& row : maze) {
-            triviallyReachable.emplace_back();
-            for (int i = 0; i < row.size(); ++i) {
-                triviallyReachable.back().emplace_back(PipeType::NONE);
-            }
-        }
-
-        int x = startX;
-        int y = startY;
-        auto cameFrom = Direction::NONE;
-        do {
-            auto& place = maze.at(x, y);
-            triviallyReachable.at(x, y) = SearchablePipeSegment{place.type()};
-            cameFrom = tryTravel(place, cameFrom);
-            switch (cameFrom) {
-                case Direction::UP:     y--; cameFrom = Direction::DOWN;    break;
-                case Direction::DOWN:   y++; cameFrom = Direction::UP;      break;
-                case Direction::LEFT:   x--; cameFrom = Direction::RIGHT;   break;
-                case Direction::RIGHT:  x++; cameFrom = Direction::LEFT;    break;
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "UnreachableCode"
-                default: throw std::logic_error("Impossible travel direction given by tryTravel().");
-#pragma clang diagnostic pop
-            }
-        } while (! (x == startX && y == startY));
-
-        BFSReachableTiles(triviallyReachable);
-
+        std::cout << "final count\t";
         int enclosedTiles = 0;
         for (int i = 1; i < shrunkDown.size() - 1; ++i) {
             for (int j = 1; j < shrunkDown[i].size() - 1; ++j) {
