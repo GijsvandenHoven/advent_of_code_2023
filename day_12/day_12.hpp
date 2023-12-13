664f4bb054a997aef813db5505371b96e097efc6
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-13 08:48:53 +0100

    much less scuffed recursive backtracker. No more mutation of data, no more mountains of cringe code.

--- day_12/day_12.hpp
+++ day_12/day_12.hpp
@@ -28,207 +28,58 @@ struct SpringRecord {
     [[nodiscard]] int64_t countPossibleRecords() const {
         int recordIndex = 0;
         int numbersIndex = 0;
-        std::vector<char> recordMemory;
-        recordMemory.reserve(data.size());
+        int currentBlockSize = 0;
 
-
-        int64_t cachelessResult = recursiveCount(recordIndex, numbersIndex, recordMemory);
-        recordMemory.clear();
-        int64_t result = recursiveCount(recordIndex, numbersIndex, recordMemory);
-
-        if (cachelessResult != result) {
-            std::cout << "Correct: " << cachelessResult << ", cache: " << result << "\n";
-            std::cout << '\t' << data << "\n";
-        }
-
-        return cachelessResult;
+        auto res = recursiveCount(recordIndex, numbersIndex, currentBlockSize);
+        std::cout << res << "\n";
+        return res;
     }
 
-    static void strmem(const std::vector<char>& m) {
-        std::cout << "mem: "; for (auto&& c : m) std::cout << c; std::cout << "\n";
-    }
+    [[nodiscard]] int64_t recursiveCount(int recordIndex, int numbersIndex, int currentBlockSize) const {
+        if (recordIndex == data.size()) {
+            bool blockTerminatedLegal = numbersIndex == numbers.size() - 1 && currentBlockSize == numbers.back(); // 1 block left but it's finished, actually.
+            bool dotTerminatedLegal = numbersIndex == numbers.size() && currentBlockSize == 0; // out of blocks and no ongoing one.
 
-    [[nodiscard]] int64_t recursiveCount(
-            int recordIndex,
-            int numbersIndex,
-            std::vector<char>& recordMemory
-    ) const {
-        if (recordIndex >= data.size()) {
-            // std::cout << "end reached .. "; strmem(recordMemory);
-            bool ok = sequenceIsLegal(recordMemory); // recursion base case. This sequence is maybe possible.
-
-            return ok; // 0 or 1 is intentional here. it's either possible, or not...
+            return blockTerminatedLegal || dotTerminatedLegal;
         }
 
         char c = data[recordIndex];
-        switch(c) {
-            case '.': {
-                auto [cont, retVal] = handleDot(numbersIndex, recordMemory);
-                if (!cont) return retVal;
-                break;
-            }
-            case '#': {
-                auto [cont, retVal] = handlePound(numbersIndex, recordIndex, recordMemory);
-                if (!cont) return retVal;
-                break;
-            }
-            case '?': {
-                return handleQuestion(numbersIndex, recordIndex, recordMemory);
-            }
-            default: throw std::logic_error("Impossible char.");
-        }
 
-        // so far so good. let's try the next character.
-        return recursiveCount(recordIndex + 1, numbersIndex, recordMemory);
-    }
+        switch (c) {
+            case '#':
+                if (numbersIndex == numbers.size()) { // out of blocks, yet trying to add to it? impossible.
+                    return 0;
+                } else if (currentBlockSize == numbers[numbersIndex]) { // adding to the block would overflow here.
+                    return 0;
+                } else { // expand block (or start one)
+                    return recursiveCount(recordIndex + 1, numbersIndex, currentBlockSize + 1);
+                }
+            case '.':
+                if (currentBlockSize != 0 && currentBlockSize != numbers[numbersIndex]) { // end of existing block but not the right size.
+                    return 0;
+                } else if (currentBlockSize == 0) { // no ongoing block.
+                    return recursiveCount(recordIndex + 1, numbersIndex, 0);
+                } else if (numbersIndex == numbers.size()) { // nonzero blocksize, but out of numbers? illegal.
+                    return 0;
+                } else { // nonzero blocksize, so this '.' finishes the block.
+                    return recursiveCount(recordIndex + 1, numbersIndex + 1, 0);
+                }
+            case '?':
+                if (numbersIndex == numbers.size()) { // out of spring blocks, this can only be a .
+                    return recursiveCount(recordIndex + 1, numbersIndex, 0);
+                } else if (currentBlockSize == numbers[numbersIndex]) { // must be a ., or we violate the block size. Also start a new block.
+                    return recursiveCount(recordIndex + 1, numbersIndex + 1, 0);
+                } else if (currentBlockSize > 0 && currentBlockSize < numbers[numbersIndex]) { // ongoing block must be continued.
+                    return recursiveCount(recordIndex + 1, numbersIndex, currentBlockSize + 1);
+                } else { // no ongoing block, could be both. So try both.
+                    if (currentBlockSize != 0) throw std::logic_error("nonzero block size?");
 
-    [[nodiscard]] bool sequenceIsLegal(const std::vector<char>& partialSequence) const {
+                    auto a = recursiveCount(recordIndex + 1, numbersIndex, 1);
+                    auto b = recursiveCount(recordIndex + 1, numbersIndex, 0);
 
-        if (partialSequence.size() > data.size()) return false;
-
-        int numbersIndex = 0;
-        int currentSequenceCount = 0;
-        for (int i = 0; i < partialSequence.size(); ++i) {
-            switch(partialSequence[i]) {
-                case '#':
-                    if (numbersIndex >= numbers.size()) {
-                        return false;
-                    }
-                    currentSequenceCount++;
-                    break;
-                case '.':
-                    if (i > 0 && partialSequence[i-1] == '#') { // sequence ended.
-                        if (currentSequenceCount != numbers[numbersIndex]) { // and it's not a match.
-                            return false;
-                        }
-                        currentSequenceCount = 0;
-                        numbersIndex++;
-                    }
-                    break;
-                default: throw std::logic_error("Impossible char in partial sequence.");
-            }
-        }
-
-        if (partialSequence.size() == data.size()) {
-            if (partialSequence.back() == '#') {
-                return numbersIndex == numbers.size() - 1 && currentSequenceCount == numbers[numbersIndex];
-            } else {
-                return numbersIndex == numbers.size();
-            }
-        } else {
-            if (partialSequence.back() == '#') {
-                return numbersIndex <= numbers.size() - 1 && currentSequenceCount <= numbers[numbersIndex]; // there might be more # after.
-            } else {
-                return true; // looks good so far.
-            }
-        }
-    }
-
-    [[nodiscard]] std::pair<bool, int64_t> handleDot(int& numbersIndex, std::vector<char>& recordMemory) const {
-        if (recordMemory.back() == '#') { // sequence of numbers end.
-            // was this sequence as expected?
-            int expected = numbers[numbersIndex];
-            int count = 0;
-            for (auto it = recordMemory.rbegin(); it != recordMemory.rend() && (*it == '#'); ++it) {
-                ++count;
-            }
-            if (count != expected) {
-                return std::make_pair(false, 0);
-            }
-
-            numbersIndex++; // go to the next number since this sequence is completed.
-        }
-        recordMemory.emplace_back('.');
-        return std::make_pair(true, 0);
-    }
-
-    [[nodiscard]] std::pair<bool, int64_t> handlePound(int& numbersIndex, int& recordIndex, std::vector<char>& recordMemory) const {
-        if (numbersIndex >= numbers.size()) { // found another sequence, but we are out of numbers representing them. Impossible.
-            return std::make_pair(false, 0);
-        }
-        recordMemory.emplace_back('#');
-        // check legality of this by comparing the current cluster to the recent number.
-        int seqEnd = recordIndex + 1;
-        while (seqEnd < data.size() && data[seqEnd] == '#') { // check the data if the future is still possible.
-            seqEnd++;
-        }
-        int seqStart = recordIndex - 1;
-        while (seqStart >= 0 && recordMemory[seqStart] == '#') { // check the memory, for decisions we made in the past.
-            seqStart--;
-        }
-        int minlen = (seqEnd - 1) - seqStart;
-        while (seqEnd < data.size() && (data[seqEnd] == '#' || data[seqEnd] == '?')) { // check more if it's possible.
-            seqEnd++;
-        }
-        int maxlen = (seqEnd - 1) - seqStart;
-
-        int recentNumber = numbers[numbersIndex];
-        if (recentNumber < minlen || recentNumber > maxlen) {
-            return std::make_pair(false, 0); // We are on a path of creating an impossible sequence.
-        }
-
-        return std::make_pair(true, 0);
-    }
-
-    [[nodiscard]] int64_t handleQuestion(int& numbersIndex, int& recordIndex, std::vector<char>& recordMemory) const {
-        int64_t totalPossible = 0;
-        // let's try '#', how much is possible?
-        {
-            recordMemory.emplace_back('#');
-            bool legal = sequenceIsLegal(recordMemory);
-            // std::cout << "\t\tRecurse on ? with '#', is it legal? " << legal << "\n";
-            if (legal) {
-                // is the result of placing # here known to the cache?
-                int64_t options = recursiveCount(
-                        recordIndex + 1,
-                        numbersIndex,
-                        recordMemory
-                );
-
-                totalPossible += options;
-            }
-            // clean up after recursion (or just illegal adding of a '.'), recordMemory was mutated.
-            // We want a memory from 0 up to but not incl. recordIndex.
-            recordMemory.erase(recordMemory.begin() + recordIndex, recordMemory.end());
-        }
-        // let's try '.', how much is possible?
-        {
-            bool sequenceEnder = recordMemory.back() == '#';
-            bool legal = true;
-            recordMemory.emplace_back('.');
-            // check legality of this move.
-            if (sequenceEnder) {
-                legal = sequenceIsLegal(recordMemory);
-            }
-
-            if (legal) { // filled in a ., continue ahead to count possibilities.
-
-                int64_t options = recursiveCount(
-                        recordIndex + 1,
-                        numbersIndex + sequenceEnder,
-                        recordMemory
-                );
-
-                totalPossible += options;
-            }
-            // clean up after recursion (or just illegal adding of a '.'), recordMemory was mutated.
-            // We want a memory from 0 up to but not incl. recordIndex.
-            recordMemory.erase(recordMemory.begin() + recordIndex, recordMemory.end());
-        }
-
-        return totalPossible;
-    }
-
-    static void updateCache(const std::vector<char>& recordMemory, std::vector<std::vector<int>>& cache) {
-        //std::cout << "\tcaching... "; strmem(recordMemory); std::cout << "\n";
-        int poundSymbol = 0;
-        for (int i = 0; i < recordMemory.size(); ++i) {
-            if (recordMemory[i] == '#') {
-                int& value = cache[poundSymbol][i];
-                value = (value == -1 ? 1 : value + 1);
-                //std::cout << "\t\twrite " << value << " to " << poundSymbol << ", " << i << "\n";
-                poundSymbol++;
-            }
+                    return a + b;
+                }
+            default: throw std::logic_error("Unexpected char in recursive count.");
         }
     }
 };
