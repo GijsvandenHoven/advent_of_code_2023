37b4623abbef4aad9fdd2bb367c0f5af4c066c85
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-21 19:59:50 +0100

    cleanup, move constexpr derivation to the top, add preprocessor directives for toggling print/algo compare features.

--- day_21/day_21.hpp
+++ day_21/day_21.hpp
@@ -7,7 +7,70 @@
 
 #define DAY 21
 
+// printing parts of the derivation / calculation process.
+#define DO_P2_COUT false
+
+// instead of using the problem input,
+// use an empty grid for comparign with the constexpr derivation & the bruteforce reference value.
+// The solution reported is 1 if equal (what we want to see), 0 otherwise.
+#define DO_P2_EMPTY_GRID_ALGO_COMPARE false
+
 NAMESPACE_DEF(DAY) {
+
+// Mathematical derivation of the input size given an empty grid.
+// The formula derived is re-used in part 2,
+// where the formulae for volumes on full, tippy, inner & outer corner grids are replaced by numbers found by BFS.
+constexpr int GRID_SIZE = 131;
+constexpr int N_STEPS = 26501365;
+
+// grid size should be an odd number.
+static_assert(GRID_SIZE % 2 == 1);
+
+// nsteps - floor(gridsize/2) should be perfectly divisible by grid size.
+static_assert((N_STEPS - (GRID_SIZE / 2)) % GRID_SIZE == 0);
+
+constexpr int64_t N_FULL_GRIDS_PER_DIRECTION = (N_STEPS - (GRID_SIZE / 2)) / GRID_SIZE - 1;
+constexpr int64_t N_RINGS_OF_FULL_GRIDS = N_FULL_GRIDS_PER_DIRECTION; // cheeky little fact about that.
+
+// we are assuming the perimeter is oddly matched, so the outer complete grid ring is evenly matched.
+static_assert(N_RINGS_OF_FULL_GRIDS % 2 == 1 && N_STEPS % 2 == 1);
+
+constexpr int64_t N_EVEN_GRID_RINGS = (N_RINGS_OF_FULL_GRIDS / 2) + 1;
+constexpr int64_t N_ODD_GRID_RINGS = N_RINGS_OF_FULL_GRIDS / 2;
+
+// for reference on what is really going on.
+constexpr auto PYRAMID_VOLUME = [](int64_t n) constexpr -> int64_t { return ((n+1)*n) / 2; };
+constexpr auto ODD_GRID_COUNT_FORMULA = [](int64_t n_grid_rings) constexpr -> int64_t {
+    return 1 + 8 * PYRAMID_VOLUME(n_grid_rings);
+};
+constexpr auto EVEN_GRID_COUNT_FORMULA = [](int64_t n_grid_rings) constexpr-> int64_t {
+    return 4 * n_grid_rings + 8 * PYRAMID_VOLUME(n_grid_rings-1);
+};
+
+constexpr int64_t ODD_GRID_COUNT = ODD_GRID_COUNT_FORMULA(N_ODD_GRID_RINGS);
+constexpr int64_t EVEN_GRID_COUNT = EVEN_GRID_COUNT_FORMULA(N_EVEN_GRID_RINGS);
+
+constexpr int64_t N_EVEN_PLACES_IN_EMPTY_GRID = GRID_SIZE * GRID_SIZE / 2 + 1; // works because we asserted grid_size is an odd number.
+constexpr int64_t N_ODD_PLACES_IN_EMPTY_GRID = GRID_SIZE * GRID_SIZE / 2;
+
+constexpr int64_t N_PLACES_IN_FULL_GRIDS = N_ODD_PLACES_IN_EMPTY_GRID * ODD_GRID_COUNT + N_EVEN_PLACES_IN_EMPTY_GRID * EVEN_GRID_COUNT;
+
+constexpr int64_t N_CORNERS_PER_SIDE = N_FULL_GRIDS_PER_DIRECTION * 2 + 1; // an inner and outer per tile, plus 1 inner for tippy.
+constexpr int64_t N_INNER_PER_SIDE = N_CORNERS_PER_SIDE / 2 + 1;
+constexpr int64_t N_OUTER_PER_SIDE = N_CORNERS_PER_SIDE / 2;
+
+// for a grid, the value of tiles per inner/outer is the same on each side, so we can take shortcuts here.
+auto SUM_N_DECR_BY_2 = [](int64_t n) constexpr -> int64_t { return (n+1)*(n+1)/4; };
+constexpr int64_t N_INNER_CORNER_PLACES_IN_EMPTY_GRID = SUM_N_DECR_BY_2(GRID_SIZE/2); // (k+1)(k+1)/4
+constexpr int64_t N_OUTER_CORNER_PLACES_IN_EMPTY_GRID = N_ODD_PLACES_IN_EMPTY_GRID - SUM_N_DECR_BY_2(GRID_SIZE/2-1);
+
+constexpr int64_t N_TILES_ON_INNER_CORNERS = N_INNER_CORNER_PLACES_IN_EMPTY_GRID * N_INNER_PER_SIDE * 4;
+constexpr int64_t N_TILES_ON_OUTER_CORNERS = N_OUTER_CORNER_PLACES_IN_EMPTY_GRID * N_OUTER_PER_SIDE * 4;
+
+constexpr int64_t N_TIPPY_TILES = 4 * (N_ODD_PLACES_IN_EMPTY_GRID - 2 * SUM_N_DECR_BY_2(GRID_SIZE/2-1));
+
+constexpr int64_t TOTAL_IF_EMPTY_GRID = N_TILES_ON_INNER_CORNERS + N_TILES_ON_OUTER_CORNERS + N_PLACES_IN_FULL_GRIDS + N_TIPPY_TILES;
+
 
 struct Tile {
     bool occupied;
@@ -193,80 +256,33 @@ public:
 
     void v2() const override {
 
-        constexpr int GRID_SIZE = 131;
-        constexpr int N_STEPS = 26501365;
+#if DO_P2_COUT
+        std::cout << "CONSTEXPR = " << TOTAL_IF_EMPTY_GRID << "\n";
+#endif
 
-        // grid size should be an odd number.
-        static_assert(GRID_SIZE % 2 == 1);
+        MutableGarden _seed_;
+#if DO_P2_EMPTY_GRID_ALGO_COMPARE
+        MutableGarden::makeBlankGrid(GRID_SIZE, _seed_);
+#else
+        grid.mutableCopy(_seed_);
+#endif
+        const MutableGarden blueprint = std::move(_seed_);
 
-        // nsteps - floor(gridsize/2) should be perfectly divisible by grid size.
-        static_assert((N_STEPS - (GRID_SIZE / 2)) % GRID_SIZE == 0);
-
-        constexpr int64_t N_FULL_GRIDS_PER_DIRECTION = (N_STEPS - (GRID_SIZE / 2)) / GRID_SIZE - 1;
-        constexpr int64_t N_RINGS_OF_FULL_GRIDS = N_FULL_GRIDS_PER_DIRECTION; // cheeky little fact about that.
-
-        // we are assuming the perimeter is oddly matched, so the outer complete grid ring is evenly matched.
-        static_assert(N_RINGS_OF_FULL_GRIDS % 2 == 1 && N_STEPS % 2 == 1);
-
-        constexpr int64_t N_EVEN_GRID_RINGS = (N_RINGS_OF_FULL_GRIDS / 2) + 1;
-        constexpr int64_t N_ODD_GRID_RINGS = N_RINGS_OF_FULL_GRIDS / 2;
-
-        // for reference on what is really going on.
-        constexpr auto PYRAMID_VOLUME = [](int64_t n) constexpr -> int64_t { return ((n+1)*n) / 2; };
-        constexpr auto ODD_GRID_COUNT_FORMULA = [PYRAMID_VOLUME](int64_t n_grid_rings) constexpr -> int64_t {
-            return 1 + 8 * PYRAMID_VOLUME(n_grid_rings);
-        };
-        constexpr auto EVEN_GRID_COUNT_FORMULA = [PYRAMID_VOLUME](int64_t n_grid_rings) constexpr-> int64_t {
-            return 4 * n_grid_rings + 8 * PYRAMID_VOLUME(n_grid_rings-1);
-        };
-
-        constexpr int64_t ODD_GRID_COUNT = ODD_GRID_COUNT_FORMULA(N_ODD_GRID_RINGS);
-        constexpr int64_t EVEN_GRID_COUNT = EVEN_GRID_COUNT_FORMULA(N_EVEN_GRID_RINGS);
-
-        constexpr int64_t N_EVEN_PLACES_IN_EMPTY_GRID = GRID_SIZE * GRID_SIZE / 2 + 1; // works because we asserted grid_size is an odd number.
-        constexpr int64_t N_ODD_PLACES_IN_EMPTY_GRID = GRID_SIZE * GRID_SIZE / 2;
-
-        constexpr int64_t N_PLACES_IN_FULL_GRIDS = N_ODD_PLACES_IN_EMPTY_GRID * ODD_GRID_COUNT + N_EVEN_PLACES_IN_EMPTY_GRID * EVEN_GRID_COUNT;
-
-        constexpr int64_t N_CORNERS_PER_SIDE = N_FULL_GRIDS_PER_DIRECTION * 2 + 1; // an inner and outer per tile, plus 1 inner for tippy.
-        constexpr int64_t N_INNER_PER_SIDE = N_CORNERS_PER_SIDE / 2 + 1;
-        constexpr int64_t N_OUTER_PER_SIDE = N_CORNERS_PER_SIDE / 2;
-
-        // for a grid, the value of tiles per inner/outer is the same on each side, so we can take shortcuts here.
-        // todo: notice me, scary, might not generalize for any empty grid size and n_steps that passes the assertions.
-        auto SUM_N_DECR_BY_2 = [](int64_t n) constexpr -> int64_t { return (n+1)*(n+1)/4; };
-        constexpr int64_t N_INNER_CORNER_PLACES_IN_EMPTY_GRID = SUM_N_DECR_BY_2(GRID_SIZE/2); // (k+1)(k+1)/4
-        constexpr int64_t N_OUTER_CORNER_PLACES_IN_EMPTY_GRID = N_ODD_PLACES_IN_EMPTY_GRID - SUM_N_DECR_BY_2(GRID_SIZE/2-1);
-
-        constexpr int64_t N_TILES_ON_INNER_CORNERS = N_INNER_CORNER_PLACES_IN_EMPTY_GRID * N_INNER_PER_SIDE * 4;
-        constexpr int64_t N_TILES_ON_OUTER_CORNERS = N_OUTER_CORNER_PLACES_IN_EMPTY_GRID * N_OUTER_PER_SIDE * 4;
-
-        constexpr int64_t N_TIPPY_TILES = 4 * (N_ODD_PLACES_IN_EMPTY_GRID - 2 * SUM_N_DECR_BY_2(GRID_SIZE/2-1));
-
-        constexpr int64_t TOTAL = N_TILES_ON_INNER_CORNERS + N_TILES_ON_OUTER_CORNERS + N_PLACES_IN_FULL_GRIDS + N_TIPPY_TILES;
-
-        std::cout << "CONSTEXPR = " << TOTAL << "\n";
-
+#if DO_P2_COUT
         if constexpr (N_STEPS < 1000) {
             MutableGarden reference;
             MutableGarden::makeBlankGrid(2 * N_STEPS + 1, reference);
             BFS(reference, N_STEPS, N_STEPS);
-            std::cout << "REF VALUE = " << reference.testReachability(N_STEPS) << "\n";
+            std::cout << "BRUTEFORCE REF VALUE = " << reference.testReachability(N_STEPS) << "\n";
         } else {
-            std::cout << "NO REF VALUE, TOO LARGE FOR BRUTE FORCE\n";
+            std::cout << "NO BRUTEFORCE REF VALUE, TOO LARGE\n";
         }
+#endif
 
-        MutableGarden full_grid;
-        MutableGarden::makeBlankGrid(GRID_SIZE, full_grid);
+        MutableGarden full_grid = blueprint;
         BFS(full_grid, GRID_SIZE/2, GRID_SIZE/2);
         int reachable_odd = full_grid.testReachability(GRID_SIZE);
         int reachable_even = full_grid.testReachability(GRID_SIZE+1);
-
-        std::cout << "ro " << reachable_odd << ", re " << reachable_even << "\n";
-
-        MutableGarden copy;
-        MutableGarden::makeBlankGrid(GRID_SIZE, copy);
-        const MutableGarden blueprint = std::move(copy);
 
         auto reachabilityFrom = [&blueprint](int startX, int startY, int n_steps) -> int {
             auto worksheet = blueprint; // copy the blueprint into a version we can mutate.
@@ -283,9 +299,7 @@ public:
         // bottom tippy enters from mid-top, and has GRID_SIZE-1 steps left.
         int64_t bottomTippyReach = reachabilityFrom(GRID_SIZE/2, 0, GRID_SIZE-1);
 
-        std::cout << "tippies: " << leftTippyReach << ", " << rightTippyReach << ", " << topTippyReach << ", " << bottomTippyReach << "\n";
         int64_t tippyReachSum = leftTippyReach + rightTippyReach + topTippyReach + bottomTippyReach;
-        std::cout << "\ttippySum: " << tippyReachSum << "\n";
 
         // top-right inner corner enters from top right, and has GRID_SIZE/2-1 steps left. (Half lost from moving to the corner in a straight line)
         int64_t TRInnerReach = reachabilityFrom(GRID_SIZE-1, 0, GRID_SIZE/2-1);
@@ -307,9 +321,6 @@ public:
         // bot-right outer corner enters from bottom right, and has GRID_SIZE+GRID_SIZE/2-1 steps left.
         int64_t BROuterReach = reachabilityFrom(GRID_SIZE-1, GRID_SIZE-1, GRID_SIZE+GRID_SIZE/2-1);
 
-        std::cout << "outers: " << BROuterReach << ", " << BLOuterReach << ", " << TLOuterReach << ", " << TROuterReach << "\n";
-        std::cout << "inners: " << BRInnerReach << ", " << BLInnerReach << ", " << TLInnerReach << ", " << TRInnerReach << "\n";
-
         int64_t grid_reach = (N_STEPS - GRID_SIZE/2) / GRID_SIZE;  // with steps as what remains after escaping the core. how many grids can we span?
         int64_t full_grid_reach = grid_reach - 1; // the last grid is not full, it's a tippy. The 'central' grid is also not counted.
 
@@ -323,11 +334,7 @@ public:
         int64_t full_grid_total_even = 4 * full_grid_reach_even_partition + 8 * PYRAMID_VOLUME(full_grid_reach_even_partition - 1);
         int64_t full_grid_total_odd = 1 /*core*/ + 8 * PYRAMID_VOLUME(full_grid_reach_odd_partition);
 
-        std::cout << "fgte " << full_grid_total_even << ", fgto: " << full_grid_total_odd << "\n";
-
         int64_t fullGridSum = full_grid_total_even * reachable_even + full_grid_total_odd * reachable_odd;
-
-        std::cout << "fgs " << fullGridSum << "\n";
 
         int64_t n_inner_corners_per_side = grid_reach; // every grid including tippy has one matched inner corner per side.
         int64_t n_outer_corners_per_side = full_grid_reach; // every grid except tippy has one matched outer corner per side.
@@ -344,21 +351,40 @@ public:
         int64_t n_tiles_in_inner_BR = BRInnerReach * n_inner_corners_per_side;
         int64_t n_tiles_in_outer_BR = BROuterReach * n_outer_corners_per_side;
 
-        std::cout << "inners: BR " << n_tiles_in_inner_BR << ", BL " << n_tiles_in_inner_BL << ", TL " << n_tiles_in_inner_TL << ", TR " << n_tiles_in_inner_TR << "\n";
-        std::cout << "outers: BR " << n_tiles_in_outer_BR << ", BL " << n_tiles_in_outer_BL << ", TL " << n_tiles_in_outer_TL << ", TR " << n_tiles_in_outer_TR << "\n";
-
         int64_t innerSum = n_tiles_in_inner_BR + n_tiles_in_inner_BL + n_tiles_in_inner_TR + n_tiles_in_inner_TL;
         int64_t outerSum = n_tiles_in_outer_BR + n_tiles_in_outer_BL + n_tiles_in_outer_TR + n_tiles_in_outer_TL;
 
         int64_t cornerSum = innerSum + outerSum;
 
+#if DO_P2_COUT
+        std::cout << "ro " << reachable_odd << ", re " << reachable_even << "\n";
+        std::cout << "tippies: " << leftTippyReach << ", " << rightTippyReach << ", " << topTippyReach << ", " << bottomTippyReach << "\n";
+        std::cout << "\ttippySum: " << tippyReachSum << "\n";
+        std::cout << "outers: " << BROuterReach << ", " << BLOuterReach << ", " << TLOuterReach << ", " << TROuterReach << "\n";
+        std::cout << "inners: " << BRInnerReach << ", " << BLInnerReach << ", " << TLInnerReach << ", " << TRInnerReach << "\n";
+        std::cout << "fgte " << full_grid_total_even << ", fgto: " << full_grid_total_odd << "\n";
+        std::cout << "fgs " << fullGridSum << "\n";
+        std::cout << "inners: BR " << n_tiles_in_inner_BR << ", BL " << n_tiles_in_inner_BL << ", TL " << n_tiles_in_inner_TL << ", TR " << n_tiles_in_inner_TR << "\n";
+        std::cout << "outers: BR " << n_tiles_in_outer_BR << ", BL " << n_tiles_in_outer_BL << ", TL " << n_tiles_in_outer_TL << ", TR " << n_tiles_in_outer_TR << "\n";
         std::cout << "Corner sum " << cornerSum << "\n";
-
-        reportSolution(cornerSum + tippyReachSum + fullGridSum);
+#endif
+#if DO_P2_EMPTY_GRID_ALGO_COMPARE
+        reportSolution(TOTAL_IF_EMPTY_GRID == cornerSum + tippyReachSum + fullGridSum);
+#else
+        int64_t solution = cornerSum + tippyReachSum + fullGridSum;
+        if (solution > TOTAL_IF_EMPTY_GRID) {
+            throw std::logic_error(
+                    "solution exceeds upper bound of " + std::to_string(TOTAL_IF_EMPTY_GRID)
+                    + " Is the constexpr upper bound correct? grid size: "
+                    + std::to_string(GRID_SIZE) + ", N_STEPS: " + std::to_string(N_STEPS) + "."
+            );
+        }
+        reportSolution(solution);
+#endif
     }
 
     void parseBenchReset() override {
-
+        grid = BlueprintGarden(); // private inheritance, no clear. So let's just yeet it all.
     }
 
 private:
@@ -392,3 +418,5 @@ private:
 } // namespace
 
 #undef DAY
+#undef DO_P2_COUT
+#undef DO_P2_EMPTY_GRID_ALGO_COMPARE
