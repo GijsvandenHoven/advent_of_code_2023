f11598e002f9f9dacb973217bb8eb70ebf656689
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-12-24 07:31:43 +0100

    remove the old function

--- day_23/day_23.hpp
+++ day_23/day_23.hpp
@@ -68,19 +68,10 @@ public:
 
         // reduce the graph by collapsing the labyrinth sections into "blocks" with appropriate length.
         BlockCache cache;
-        auto iterToStart = calcBlock(x, y, Direction::SOUTH, cache);
-
+        auto iterToStart = neoCalcBlock(x, y + 1, Direction::SOUTH, cache);
         int longest = calcLongestPath(**iterToStart);
-        std::cout << "ref case " << longest-1 << "\n";
-        cache.clear();
 
-        BlockCache neoCache;
-        auto iterToNeoStart = neoCalcBlock(x, y + 1, Direction::SOUTH, neoCache);
-        int neoLongest = calcLongestPath(**iterToNeoStart);
-
-        std::cout << **iterToNeoStart << "\n";
-
-        reportSolution(neoLongest);
+        reportSolution(longest);
     }
 
     void v2() const override {
@@ -105,6 +96,12 @@ private:
         return first.cost + max;
     }
 
+    /**
+     * Walk to the successor incrementing a count until...
+     * ...a direction marker is reached, at which point,
+     * Recursively push onto the successor list calls to this function.
+     * Care is taken only "legal" directions are walked, i.e. not passing through one-ways.
+     */
     BlockCache::iterator neoCalcBlock(const int x, const int y, const Direction facing, BlockCache& cache) const {
         auto block = std::make_shared<Block>(x, y);
         block->startX = x;
@@ -120,14 +117,8 @@ private:
         bool pathToWalk = true;
         while (pathToWalk) {
             steps++;
-
-            //std::cout << "at " << cx << ", " << cy << "\n";
             std::array<std::pair<int,int>, 4> options;
             int count = numberOfPaths(cx, cy, options);
-            for (int i = 0; i < count; ++i) {
-                auto opt = options[i];
-                //std::cout << "option: " << opt.first << ", " << opt.second << "\n";
-            }
 
             Direction oppositeFacing;
             switch (cfacing) {
@@ -150,7 +141,6 @@ private:
             for (int i = 0; i < count-1; ++i) { // for everything in the valid portion of the array minus the banished element
                 auto [nx, ny] = options[i];
                 char next = grid[ny][nx];
-                //std::cout << "from " << cx << ", " << cy << " try " << nx << ", " << ny << "\n";
                 auto potentialFacing = whatIsFacing(nx, ny, cx, cy);
 
                 if (next != PATH_MARKER) {
@@ -189,8 +179,6 @@ private:
                             target.successors.emplace_back(*iterToCache);
                         }
 
-                        //pathToWalk = false; // end of loop.
-
                     } else { // intersection square
                         //std::cout << "\tIntersection after " << steps << "\n";
                         if (steps != 1) throw std::logic_error("Intersection square but steps not 1.");
@@ -216,8 +204,6 @@ private:
                         } else {
                             target.successors.emplace_back(*iterToCache);
                         }
-
-                        //pathToWalk = false;
                     }
                 } else {
                     if (count != 2) throw std::logic_error("Path marker & Option inconsistency.");
@@ -227,124 +213,9 @@ private:
                     cy = ny;
                 }
             }
-
-            //std::cout << "end of for loop with " << cx << ", " << cy << "\n";
         }
-
-        //std::cout << "end of while loop with " << cx << ", " << cy << "\n";
 
         return iter;
-    }
-
-    /**
-     * Walk to the successor incrementing a count until...
-     * ...a direction marker is reached, at which point,
-     * Recursively push onto the successor list calls to this function.
-     */
-    BlockCache::iterator calcBlock(int x, int y, Direction facing, BlockCache& cache) const {
-        auto block = std::make_shared<Block>(x, y);
-        block->startX = x;
-        block->startY = y;
-        auto [iter, success] = cache.emplace(std::move(block));
-        if (! success) throw std::logic_error("Inserting block which already exists. " + std::to_string(x) + ", " + std::to_string(y));
-        auto& target = **iter;
-
-        int steps = 1; // including the current x, y.
-        do {
-            auto [nx, ny, nfacing] = successor(x, y, facing);
-            x = nx;
-            y = ny;
-            facing = nfacing;
-            ++steps;
-
-            if (grid[y][x] == END_MARKER) { // we are done here, write the cost and exit this recursion.
-                target.cost = steps;
-                break;
-            }
-
-            // Intersection square logic.
-            if (grid[y][x] != PATH_MARKER) {
-                auto [interX, interY, _] = successor(x, y, facing); // we are now on the intersection
-                ++steps;
-                target.cost = steps;
-                std::array<std::pair<int,int>,4> possible{{ {interX,interY-1}, {interX,interY+1}, {interX-1,interY}, {interX+1,interY} }};
-                for (auto& [px, py] : possible) {
-                    if (px == x && py == y) continue;
-
-                    auto possibleFacing = whatIsFacing(px, py, interX, interY);
-
-                    char prospect = grid[py][px];
-                    switch (prospect) {
-                        case PATH_MARKER: [[fallthrough]]; // This should be an intersection of one-ways!
-                        default: throw std::logic_error("Unexpected char in grid");
-                        case WALL_MARKER: continue; // do not recurse here.
-                        case '>': if (possibleFacing == Direction::EAST) break; else continue;
-                        case 'v': if (possibleFacing == Direction::SOUTH) break; else continue;
-                        case '<': if (possibleFacing == Direction::WEST) break; else continue;
-                        case '^': if (possibleFacing == Direction::NORTH) break; else continue;
-                    }
-
-                    // We have to add a successor to this block, but does it exist yet?
-                    int _x = px; int _y = py; // openMP can't capture structured bindings. And it won't compile even though there is no OMP?
-                    auto blockToMakeIter = std::find_if(cache.begin(), cache.end(), [_x, _y](auto& c){
-                        return c->startX == _x && c->startY == _y;
-                    });
-                    if (blockToMakeIter != cache.end()) {
-                        target.successors.emplace_back(*blockToMakeIter);
-                    } else {
-                        auto iteratorToNewBlock = calcBlock(px, py, possibleFacing, cache);
-                        target.successors.emplace_back(*iteratorToNewBlock);
-                    }
-                }
-
-                break; // we are done with this block now. We found the intersection square, appended blocks. No more walking fwd to do.
-            }
-        } while (grid[y][x] != END_MARKER);
-
-        // According to documentation, no iterators are invalidated by calls to emplace of std::set so this should be fine.
-        return iter;
-    }
-
-    std::tuple<int, int, Direction> successor(int x, int y, Direction facing) const {
-
-        if (grid[y][x] == END_MARKER) throw std::logic_error("Trying to find successor of end.");
-
-        std::array<std::pair<int,int>,3> inspect;
-        switch (facing) {
-            case Direction::NORTH:
-                inspect = decltype(inspect){{ {x+1,y}, {x-1,y}, {x,y-1} }}; // right left up
-                break;
-            case Direction::SOUTH:
-                inspect = decltype(inspect){{ {x+1,y}, {x-1,y}, {x,y+1} }}; // right left down
-                break;
-            case Direction::EAST:
-                inspect = decltype(inspect){{ {x+1,y}, {x,y+1}, {x,y-1} }}; // right up down
-                break;
-            case Direction::WEST:
-                inspect = decltype(inspect){{ {x-1,y}, {x,y+1}, {x,y-1} }}; // left up down
-                break;
-            default: throw std::logic_error("Unknown enum value.");
-        }
-
-        int validCount = 0;
-        int outX;
-        int outY;
-        for (auto [nx, ny] : inspect) {
-            char here = grid[ny][nx];
-            if (here != WALL_MARKER) {
-                validCount ++;
-                outX = nx;
-                outY = ny;
-            }
-        }
-
-        if (validCount != 1) {
-            throw std::logic_error("Expected exactly one successor to exist, got " + std::to_string(validCount) + " at " + std::to_string(x) + ", " + std::to_string(y) + ".");
-        }
-
-        Direction newFacing = whatIsFacing(outX, outY, x, y);
-
-        return std::make_tuple(outX, outY, newFacing);
     }
 
     static Direction whatIsFacing(int hereX, int hereY, int lastX, int lastY) {
